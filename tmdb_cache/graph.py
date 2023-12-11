1f53a26f023be4f0d46e805e0162ac23aeae2b38
Dylan Ling <setme@example.com>
2023-12-10 22:01:09 -0800

    migrate graph logic

--- tmdb_cache/graph.py
+++ tmdb_cache/graph.py
@@ -5,113 +5,69 @@ import time
 
 from itertools import chain
 from pygtrie import CharTrie
+from pprint import pprint
 
-movie_info_keys = [
-    "id",
-    "original_language",
-    "original_title",
-    "popularity",
-    "release_date",
-    "title",
-]
-
-person_info_keys = ["id", "name", "original_name", "popularity"]
+MOVIE_INFO_KEYS = ["id", "original_language", "popularity", "release_date", "title"]
+PERSON_INFO_KEYS = ["id", "name", "popularity"]
 
 
 def find_directors(crew):
     return [
-        item
+        {**item, "order": -1}
         for item in crew
         if item["department"] == "Directing" and item["job"] == "Director"
     ]
 
 
-def parse_credits(credits):
+def parse_credits(credits, keys):
     if not credits:
         return []
     cast = credits["cast"]
     directors = find_directors(credits["crew"])
-    results = directors + cast
-    return list(filter(lambda item: item, results))
+    return (
+        {
+            result.get("id"): {k: result.get(k) for k in keys}
+            for result in directors + cast
+            if result and "id" in result
+        },
+        {
+            result.get("id"): result.get("order", -1)
+            for result in directors + cast
+            if result and "id" in result
+        },
+    )
 
 
 def build_graph_from_cache():
-    start = time.time()
-    people = {}
     movies = {}
+    people = {}
+    movie_credits = {}
+    people_credits = {}
+
     for filename in os.listdir("./person"):
         with open(f"./person/{filename}", "r") as f:
-            data = json.load(f)
-            person_id = data["id"]
-            if person_id not in people:
-                people[person_id] = {}
-
-            credits = parse_credits(data)
-            people[person_id]["credits"] = {
-                item["id"]: {"order": item.get("order", -1)} for item in credits
-            }
-            for movie in credits:
-                movie_id = movie["id"]
-                if movie_id not in movies:
-                    movies[movie_id] = {}
-                if "info" not in movies[movie_id]:
-                    movies[movie_id]["info"] = {
-                        k: movie.get(k, None) for k in movie_info_keys
-                    }
-                if "credits" not in movies[movie_id]:
-                    movies[movie_id]["credits"] = {
-                        person_id: {"order": movie.get("order", -1)}
-                    }
+            person_id = filename.split("_")[0]
+            data, credits = parse_credits(json.load(f), MOVIE_INFO_KEYS)
+            movies.update(data)
+            people_credits.setdefault(person_id, {}).update(credits)
+            for movie_id, order in credits.items():
+                movie_credits.setdefault(movie_id, {}).setdefault(person_id, order)
 
     for filename in os.listdir("./movie"):
         with open(f"./movie/{filename}", "r") as f:
-            data = json.load(f)
-            movie_id = data["id"]
-            if movie_id not in movies:
-                movies["movie_id"] = {}
+            movie_id = filename.split("_")[0]
+            data, credits = parse_credits(json.load(f), PERSON_INFO_KEYS)
+            people.update(data)
+            movie_credits.setdefault(movie_id, {}).update(credits)
+            for person_id, order in credits.items():
+                people_credits.setdefault(person_id, {}).setdefault(movie_id, order)
 
-            credits = parse_credits(data)
-            movies[movie_id]["credits"] = {
-                item["id"]: {"order": item.get("order", -1)} for item in credits
-            }
-            # movies[movie_id]["credits"] = list(
-            #     map(lambda m: dict({m["id"]: m.get("order", -1)}), credits)
-            # )
-            for person in credits:
-                person_id = person["id"]
-                if person_id not in people:
-                    people[person_id] = {}
-                if "info" not in people[person_id]:
-                    people[person_id]["info"] = {
-                        k: person.get(k, None) for k in person_info_keys
-                    }
-                if "credits" not in people[person_id]:
-                    people[person_id]["credits"] = {
-                        movie_id: {"order": person.get("order", -1)}
-                    }
-
-    end = time.time()
-    print(f"Built graph from cached API calls in {end - start}s")
-    return people, movies
-
-
-def sanitize_title(title):
-    if title is None:
-        return "__unknown_title__"
-    return re.sub(r"[^a-z0-9 ]+", "", title.lower())
-
-
-def movie_titles(movies):
-    names = {}
-    for movie in movies.values():
-        if "info" not in movie:
-            continue
-        info = movie["info"]
-        release_year = (info.get("release_date") or "Unknown").split("-")[0]
-        names.setdefault(sanitize_title(info.get("title")), {}).setdefault(
-            release_year, []
-        ).append(info.get("id", None))
-    return names
+    return {
+        "movies": movies,
+        "people": people,
+        "movie_credits": movie_credits,
+        "people_credits": people_credits,
+    }
 
 
 def load_graph():
@@ -124,58 +80,19 @@ def load_graph():
     return graph
 
 
-def prefix_trie(graph):
-    start = time.time()
-    trie = CharTrie()
-    for title in graph["movie_titles"]:
-        trie[title] = graph["movie_titles"][title]
-        for prefix in ["the ", "a ", "an "]:
-            if title.startswith(prefix):
-                trie[title.lstrip(prefix)] = graph["movie_titles"][title]
-
-    end = time.time()
-    print(f"Constructed search index from graph in {end - start}s")
-
-    return trie
-
-
-def release_year(release_date):
-    if not release_date:
-        return "n/a"
-    return release_date.split("-")[0]
-
-
-def search_by_title(graph, trie, title, year=None):
-    prefix = re.sub(r"[^a-z0-9 ]+", "", title.lower())
-    try:
-        matches = trie.values(prefix)
-        movie_ids = list(
-            chain.from_iterable(
-                chain.from_iterable([match.values() for match in matches])
-            )
-        )
-        movies = [
-            graph["movies"].get(str(movie_id), {}).get("info", {})
-            for movie_id in movie_ids
-        ]
-        if year:
-            movies = list(
-                filter(
-                    lambda movie: release_year(movie.get("release_date")) == str(year),
-                    movies,
-                )
-            )
-        return sorted(movies, key=lambda movie: -movie.get("popularity", 0))
-    except:
-        return None
-
-
 def main():
     print("Constructing graph and writing to graph.json")
-    people, movies = build_graph_from_cache()
-    graph = {"people": people, "movies": movies, "movie_titles": movie_titles(movies)}
+    start = time.time()
+    graph = build_graph_from_cache()
+    end = time.time()
+    print(f"Constructed graph in {end - start}s")
+
+    print("Writing graph to json file")
+    start = time.time()
     with open("./graph.json", "w") as f:
         json.dump(graph, f)
+    end = time.time()
+    print(f"Wrote graph file in {end - start}s")
 
 
 if __name__ == "__main__":
--- tmdb_cache/recommendations.py
+++ tmdb_cache/recommendations.py
@@ -1,81 +1,120 @@
 import re
+import time
 
-from graph import load_graph, prefix_trie, release_year, search_by_title
+from graph import load_graph
 from pick import pick
 from pprint import pprint
-from itertools import chain
+from pygtrie import CharTrie
 
 
-def sanitize_title(title):
+def sanitize_title_for_trie(title):
     if title is None:
         return "__unknown_title__"
     return re.sub(r"[^a-z0-9 ]+", "", title.lower())
 
 
-def person_recommendations(graph, movie, limit=7):
-    credits = graph["movies"].get(str(movie.get("id"))).get("credits", {})
-    links = [
-        {**graph["people"].get(str(person_id), {}).get("info", {}), **data}
-        for person_id, data in credits.items()
+def movie_prefix_trie(graph):
+    start = time.time()
+    trie = CharTrie()
+    for movie in graph["movies"].values():
+        title = sanitize_title_for_trie(movie.get("title"))
+        trie[title] = movie
+        for prefix in ["the ", "a ", "an "]:
+            if title.startswith(prefix):
+                trie[title.lstrip(prefix)] = movie
+
+    end = time.time()
+    print(f"Constructed search index from graph in {end - start}s")
+
+    return trie
+
+
+def release_year(movie):
+    return movie.get("release_date", "Unknown").split("-")[0]
+
+
+def search_movie(prefix, trie, year=None):
+    prefix = re.sub(r"[^a-z0-9 ]+", "", prefix.lower())
+    try:
+        matches = trie.values(prefix)
+    except:
+        matches = []
+
+    movies = [
+        movie for movie in matches if year is None or str(year) == release_year(movie)
     ]
-    return [
-        person for person in sorted(links, key=lambda person: person.get("order", -1))
-    ][:limit]
+    return sorted(movies, key=lambda movie: -movie.get("popularity", 0))
 
 
-def movie_recommendations(graph, person, limit=7):
-    credits = graph["people"].get(str(person.get("id"))).get("credits", {})
-    return [
-        graph["movies"].get(movie_id, {}).get("info", {})
-        for movie_id, _ in sorted(
-            credits.items(),
+def format_movie(movie, with_id=False):
+    title = movie.get("title", "???")
+    year = release_year(movie)
+    movie_id = movie.get("id", "?")
+    movie_id_debug = f", id: {movie_id}" if with_id else ""
+    return f"{title} - ({year}){movie_id_debug}"
+
+
+def person_recommendations(graph, movie, limit=None):
+    people_ids = [
+        k
+        for k, _ in sorted(
+            graph["movie_credits"].get(str(movie["id"]), {}).items(),
+            key=lambda item: item[1],
+        )
+    ]
+    if limit:
+        people_ids = people_ids[:limit]
+    return [graph["people"][person_id] for person_id in people_ids]
+
+
+def movie_recommendations(graph, person, limit=None):
+    movie_ids = [
+        k
+        for k, _ in sorted(
+            graph["people_credits"].get(str(person["id"]), {}).items(),
             key=lambda item: (
-                item[1].get("order", 1000),
-                -graph["movies"].get(item[0], {}).get("info", {}).get("popularity", 0),
+                item[1],
+                -graph["movies"].get(item[0], {}).get("popularity", 0),
             ),
         )
-    ][:limit]
-
-
-def as_search_result(movie_info):
-    title = movie_info.get("title", "Unknown")
-    year = release_year(movie_info.get("release_date"))
-    movie_id = movie_info.get("id", "n/a")
-    return f"{title} ({year}) - {movie_id}"
+    ]
+    if limit:
+        movie_ids = movie_ids[:limit]
+    return [graph["movies"][movie_id] for movie_id in movie_ids]
 
 
 def main():
     graph = load_graph()
-    trie = prefix_trie(graph)
+    trie = movie_prefix_trie(graph)
     select_movie_prompt = "Matches:"
     print("Type `\q` to quit.")
-    search_input = input("\nMovie: ").strip()
+    search_input = None
     while search_input != "\q":
-        if search_input != "":
-            search_results = search_by_title(graph, trie, search_input)
-            if not search_results:
-                print("No movies found")
-            else:
-                options = list(map(as_search_result, search_results))
-
-                if len(options) > 1:
-                    option, index = pick(options, select_movie_prompt, indicator="❯")
-                    movie = search_results[index]
-                else:
-                    option = options[0]
-                    movie = search_results[0]
-
-                person_recs = person_recommendations(graph, movie)
-                movie_recs = {
-                    person.get("name", "Unknown"): movie_recommendations(graph, person)
-                    for person in person_recs
-                }
-
-                print(f"Recommendations for {option}:")
-                for person_name, movies in movie_recs.items():
-                    for movie in movies:
-                        print(f"\t{as_search_result(movie)} via {person_name}")
         search_input = input("\nMovie: ").strip()
+        if search_input == "" or search_input == "\q":
+            continue
+        movies = search_movie(search_input, trie)
+        if not movies:
+            print(f"No movies found matching `{search_input}`")
+            continue
+        options = [format_movie(movie) for movie in movies]
+        if len(options) > 1:
+            option, index = pick(options, select_movie_prompt, indicator="❯")
+            movie = movies[index]
+        else:
+            option = options[0]
+            movie = movies[0]
+        people = person_recommendations(graph, movie, limit=5)
+        movie_recs = {
+            person.get("name", "Unknown"): movie_recommendations(graph, person, limit=6)
+            for person in people
+        }
+        print(f"Recommendations for {option}:")
+        for name, movies in movie_recs.items():
+            print(f"via {name}")
+            for movie in movies:
+                print(f"\t{format_movie(movie)}")
+
     print("Goodbye!")
 
 
